b07a9c91bfeccd39611371b6e76256408165fff7
Gaetano <gaetanobarreca@gmail.com>
2023-10-25 17:28:10 -0700

    Initial commit

---
+++ src/test/java/com/store/managementapplication/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.managementapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
